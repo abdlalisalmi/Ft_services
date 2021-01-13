cf431dab470e7e48473723203791e4da38275839
Abdelaali Es salmi <aes-salm@e2r10p6.1337.ma>
2021-01-13 19:10:23 +0100

    add wordpress service

--- srcs/wordpress/test.sh
+++ srcs/wordpress/test.sh
@@ -2,4 +2,4 @@ docker stop  $(docker ps -a -q)
 docker rm  	 $(docker ps -a -q)
 docker rmi  wordpress
 docker build -t wordpress .  
-docker run -p 443:443 -p 5050:80 -it wordpress
+docker run -p 5050:80 -it wordpress
