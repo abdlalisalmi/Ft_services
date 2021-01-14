c066fe153c311590288682e09f879181d376fc61
Abdelaali Es salmi <aes-salm@e2r10p6.1337.ma>
2021-01-14 19:12:05 +0100

    update wordpress service

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -20,22 +20,22 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-// define( 'DB_NAME', 'wp_db' );
+define( 'DB_NAME', 'wordpress_db' );
 
-// /** MySQL database username */
-// define( 'DB_USER', 'admin' );
+/** MySQL database username */
+define( 'DB_USER', 'admin' );
 
-// /** MySQL database password */
-// define( 'DB_PASSWORD', 'admin' );
+/** MySQL database password */
+define( 'DB_PASSWORD', 'admin' );
 
-// /** MySQL hostname */
-// define( 'DB_HOST', 'localhost' );
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
 
-// /** Database Charset to use in creating database tables. */
-// define( 'DB_CHARSET', 'utf8' );
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
 
-// /** The Database Collate type. Don't change this if in doubt. */
-// define( 'DB_COLLATE', '' );
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
 
 /**#@+
  * Authentication Unique Keys and Salts.
