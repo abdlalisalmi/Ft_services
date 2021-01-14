004d1f75165951e1ed7625e34f462e813dd8ff82
Abdelaali Es salmi <aes-salm@e2r10p6.1337.ma>
2021-01-14 19:04:57 +0100

    update mysql service

--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -1,28 +1,23 @@
 #! /bin/bash
 
-# Wait that mysql was up
-# until mysql
-# do
-# 	echo "NO_UP"
-# done
+#create necessary folders
+mkdir -p /var/lib/mysql /run/mysqld/
 
-/usr/bin/mysql_install_db --user=mysql --datadir="/var/lib/mysql"
-/usr/bin/mysqld_safe --datadir="/var/lib/mysql"
+#install and start services
+mysql_install_db -u root > /dev/null
+exec /usr/bin/mysqld -u root &
+
+sleep 5
+
+#create db admin
+mysql -uroot -e 'CREATE DATABASE wordpress_db;'
+mysql -uroot -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;"
+mysql -uroot -e "USE wordpress_db;"
+mysql -uroot -e "DROP DATABASE test;"
+mysql -uroot -e "FLUSH PRIVILEGES;"
+mysql -uadmin -padmin  wordpress_db < wordpress_db.sql
 
 
-mysql -uroot -proot -e "CREATE DATABASE wordpress_db"
-mysql -uroot -proot -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin'"
-mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost'"
-mysql -uadmin -padmin  wordpress_db <wordpress_db.sql
-
-
-# Init DB
-# echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
-# echo "CREATE USER 'user'@'%' IDENTIFIED BY 'password';" | mysql -u root --skip-password
-# echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'user'@'%' WITH GRANT OPTION;" | mysql -u root --skip-password
-# echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
-# echo "DROP DATABASE test" | mysql -u root --skip-password
-# echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
-# mysql wordpress -u root --password=  < wordpress.sql
-
-supervisord -c /etc/supervisord.conf -s
+#avoid container to stop
+sleep infinity
